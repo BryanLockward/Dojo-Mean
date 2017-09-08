ca334751a6a34722e563ba84b4140fedf22ba6fd
Bryan Lockward <bmlockward3@verizon.net>
2017-09-07 21:42:39 -0400

    added Ninja 2

--- ninja_class.js
+++ ninja_class.js
@@ -23,8 +23,36 @@ Ninja.prototype.drinkSake = function()
 	this.health += 10;
 	return this;
 }
+Ninja.prototype.punch= function(punchee)
+{
+  if(punchee instanceof Ninja)
+  {
+    punchee.health-=5;
+    console.log(this.name +" just punched "+punchee.name +" and lost 5 health");
+  }
+  else
+  {
+    console.log("Not a Ninja");
+  }
+  return this;
+}
+Ninja.prototype.kick= function(kickee)
+{
+  if(kickee instanceof Ninja)
+  {
+    kickee.health-=15;
+    console.log(this.name +" just kicked "+kickee.name +" and lost 15 health");
+  }
+  else
+  {
+    console.log("Not a ninja");
+    return this
+  }
+}
 
 var new_ninja = new Ninja("Bryan");
-new_ninja.sayName();
-new_ninja.drinkSake();
+var new_ninja2 = new Ninja("Steve");
+new_ninja2.kick(new_ninja);
 new_ninja.showStats();
+new_ninja.punch(new_ninja2);
+new_ninja2.showStats();
